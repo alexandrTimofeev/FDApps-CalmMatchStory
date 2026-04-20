26abb22be3619042c292000e44a7e8ad6111862b
sanok52 <aleksandr.timofeev.98@bk.ru>
2026-04-20 12:17:34 +0800

    Initial commit

---
+++ Assets/Scripts/Singletons/MatchMediator.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
+using UnityEngine.Events;
+
+public class MatchMediator : GenericSingleton<MatchMediator>
+{
+    public UnityEvent<ICollection<FieldBlock>> OnMatchFound = new();
+
+    public void NotifyOfMatch(FieldBlock caller, IEnumerable<FieldBlock> fullMatch)
+    {
+        OnMatchFound?.Invoke(fullMatch.Append(caller).AsReadOnlyCollection());
+    }
+}
